a67d40da015a06a217f20e408bdd561ad459f243
Richard Spencer <richard.spencer@lifegroup.org.uk>
2022-06-03 09:57:00 +0100

    1.0.0+3 better

---
+++ android/app/src/main/kotlin/com/example/my_project/MainActivity.kt
@@ -0,0 +1,6 @@
+package uk.co.hyperbook.higranny
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/custom_code/widgets/index.dart
@@ -0,0 +1,1 @@
+export 'connect_to_room.dart' show ConnectToRoom;
